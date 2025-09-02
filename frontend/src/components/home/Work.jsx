bc859babcf0a98bb61fd21277d1bded22cd45d3c
jun-c0de <sh48520414@gmail.com>
2025-09-02 18:59:33 +0900

    ccc

--- frontend/src/components/home/Work.jsx
+++ frontend/src/components/home/Work.jsx
@@ -1,17 +1,35 @@
 import React from 'react'
-import { works } from '../../utils/works'
+import works from '../../utils/works'
+import "./styles/Work.scss"
 
 const Work = () => {
   return (
-    <div>
-      <h1 className='title-wrap'>Works.</h1>
-      <ul>
-        <li>
-
-        </li>
+    <div className='inner work-inner'>
+      <h3 className='sub-tit-2'>Works</h3>
+      <ul className="work-lst">
+        {works.map((work)=>(
+          <li key={work.id}>
+            <a href={work.links.notion} className='lst-t-wrap'>
+              <h4 className='lst-tit'>
+                {work.title}
+              </h4>
+              <div className="tags">
+                {work.tags.map((t,i)=>(
+                  <span key={i}>{t}</span>
+                ))}
+              </div>
+            </a>
+            <a href={work.links.demo} className="lst-img-wrap">
+              <div 
+              className="bg"
+              style={{backgroundImage:`url(${work.thumbnail})`}}
+              ></div>
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
-    
+
   )
 }
 
