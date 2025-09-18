b95e15dbf6b69f9dfd10893738a1015f9c646059
jun-c0de <sh48520414@gmail.com>
2025-09-18 17:15:23 +0900

    cc

--- frontend/src/components/admin/AdminContact.jsx
+++ frontend/src/components/admin/AdminContact.jsx
@@ -22,12 +22,77 @@ const AdminContact = () => {
 
     }, [])
 
+    const handleStatusUpdate = async (contactId, newStatus) => {
+        try {
+            await api.put(
+                `/api/contact/${contactId}`,
+                { status: newStatus },
+                { withCredentials: true }
+            )
+
+            setContacts((prev) =>
+                prev.map((c) => (c._id === contactId ? { ...c, status: newStatus } : c))
+            )
+            Swal.fire("수정완료", "상태가 성공적으로 수정되었습니다.", "success")
+        } catch (error) {
+            console.log("수정실패", error)
+            Swal.fire("오류 발생", "수정중 오류가 발생했습니다.", "error")
+        }
+    }
+
+    const showStatusChangeModal = async (contact) => {
+        const { value: newStatus, isConfirmed } = await Swal.fire({
+            title: "문의 상태 수정",
+            input: "radio",
+            inputOptions: {
+                "pending": "대기중",
+                "inprogress": "진행중",
+                "completed": "완료"
+            },
+            inputValue: contact.status,
+            confirmButtonText: "적용하기",
+            cancelButtonText: "취소",
+            showCancelButton: true
+        })
+
+        if (isConfirmed && newStatus) {
+            handleStatusUpdate(contact._id, newStatus)
+        }
+    }
+
+    const handleDelete = async (id) => {
+        const result = await Swal.fire({
+            title: "삭제하시겠습니까?",
+            text: "이 작업은 되돌릴 수 없습니다.",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "삭제",
+            cancelButtonText: "취소",
+        })
+
+        if(!result.isConfirmed) return;
+
+        try {
+            await api.delete(`/api/contact/${id}`,{
+                withCredentials:true
+            })
+                setContacts((prev)=>prev.filter((c)=>c._id !==id))
+                Swal.fire("삭제 완료","문의가 성공적으로 삭제되었습니다.","success")
+        } catch (error) {
+            console.log("삭제 실패",error)
+            Swal.fire("오류발생","삭제 중 문제가 발생하였습니다.","error")
+            
+        }
+    }
+
     return (
         <div className='inner admin-contact'>
             <h2>문의글 관리</h2>
             <div className="contact-wrapper">
                 <ul className="contact-list">
-                    {contacts.map((c, i) => (
+                    {contacts.map((c) => (
 
                         <li key={c._id}>
                             <p>
@@ -63,8 +128,8 @@ const AdminContact = () => {
                                 </span>
                             </p>
                             <div className="btns">
-                                <button>상태변경</button>
-                                <button>삭제</button>
+                                <button onClick={() => showStatusChangeModal(c)}>상태변경</button>
+                                <button onClick={()=>handleDelete(c._id)}>삭제</button>
                             </div>
                         </li>
                     ))}
